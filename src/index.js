a30c11dae132f8491f980dae4bfde127dfb3f02e
Liran Tal <liran.tal@gmail.com>
2020-09-03 10:17:52 +0300

    fix: show summary stats

--- src/index.js
+++ src/index.js
@@ -5,24 +5,37 @@ const audit = new Audit()
 
 module.exports = {
   onPostBuild: async ({ utils }) => {
-    debug(`detected working directory: `, process.cwd())
+    const projectDirectory = process.cwd()
+    debug(`detected working directory: `, projectDirectory)
 
     let testResults
+    let vulnerabilitiesFound = false
     try {
-      testResults = await audit.test({ directory: process.cwd() })
+      testResults = await audit.test({ directory: projectDirectory })
+      vulnerabilitiesFound = true
     } catch (error) {
       utils.build.failBuild(error)
     }
 
-    if (testResults) {
+    if (vulnerabilitiesFound && testResults) {
       const testResultsString = audit.formatOutput(testResults)
       console.log(testResultsString)
       console.log(chalk.red(`Snyk test found security vulnerabilities in production dependencies.`))
       console.log(chalk.red(`It is recommended you review before deploying`))
       utils.build.failBuild(`security vulnerabilities detected.`)
+
+      utils.status.show({
+        title: 'Snyk security test completed',
+        summary: `Security vulnerabilities were detected.`
+      })
     } else {
       console.log(chalk.green(`Snyk security test completed. no vulnerable paths found.`))
       console.log(chalk.green(`You're good to go, sir!`))
+
+      utils.status.show({
+        title: 'Snyk security scan finished',
+        summary: `Good job keeping it clean 👏`
+      })
     }
   }
 }
