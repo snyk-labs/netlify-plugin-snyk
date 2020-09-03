bb1a268a0f53010ce5892464eb46c867c9e8e92b
Liran Tal <liran.tal@gmail.com>
2020-09-03 14:31:08 +0300

    fix: utils.build.show is only used in build success

--- src/index.js
+++ src/index.js
@@ -23,11 +23,6 @@ module.exports = {
       console.log(chalk.red(`Snyk test found security vulnerabilities in production dependencies.`))
       console.log(chalk.red(`It is recommended you review before deploying`))
       utils.build.failBuild(`security vulnerabilities detected.`)
-
-      utils.status.show({
-        title: 'Snyk security test completed',
-        summary: `Security vulnerabilities were detected.`
-      })
     } else {
       console.log(chalk.green(`Snyk security test completed. no vulnerable paths found.`))
       console.log(chalk.green(`You're good to go!`))
